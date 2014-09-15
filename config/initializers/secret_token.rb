c7ca3da8b7d2e615eb906cc74cf69ef911b3c9f6
Zenen Escudero <zenen.dc.escudero@gmail.com>
2014-09-14 23:00:26 -0500

    el primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyprueba::Application.config.secret_token = 'c207ff90a1e1214e3d59247c80c5ce5831d724e1c0e5d5f521225f3b981136c3998aeac2a0e8e4617987e41b21748635918035ed164b610e2ed3a4dc94bb2893'
